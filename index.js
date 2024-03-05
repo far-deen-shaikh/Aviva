250fff9366d8512865eca5e18204c3c82a460ac7
far-deen-shaikh <fardeen1902@gmail.com>
2024-03-05 11:21:53 +0400

    Initail Commit

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get('/',(req, res) => {
     res.send("Hi Fardeen");
 });
 
+app.post('/api/zoho/aviva', (req, res) => {
+    console.log(req.body);
+
+    res.status(200).send({
+        payload: req.body,
+        message: "Data Transfer Successful"
+    });
+})
+
+
 const port = 3000;
 
 app.listen(port, () =>{
